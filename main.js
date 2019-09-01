7ad8922f0e88e57e10c7029ff8174de19a75793e
czarny-kot <53179548+czarny-kot@users.noreply.github.com>
2019-09-01 22:08:36 +0200

    Walidacja 1
    -zamiana regexu-poprzedni pozwalał na wpisanie cyfr-teraz w placeInput wpisać można wyłącznie miejsce
    -nierozwiązany pozostaje problem polskich znaków i co zrobić w przypadku ich wpisania

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 
-const incorrectCityNameInfo = "City can't contain special charakters";
-
-const doc = document.getElementById('inputCity');
-doc.addEventListener('keyup', function(e){
-    const errorText = document.getElementById('CityHelp');
-    const regW = /\W/;
+const placeInput=document.getElementById('placeInput');
+placeInput.addEventListener('input', function(e){
+    const regW =/[^A-Za-z_]/ ;
     const matchError = regW.exec(this.value);
-
-    matchError === null ? errorText.innerHTML = "" : errorText.innerHTML = incorrectCityNameInfo;
+    if(matchError===null){
+        console.log("elegancko");
+        
+    }else{
+        console.log('udało się!')
+        document.getElementById('messagesBar').style.display="block";
+        document.getElementById('messages').style.display="block";
+        document.getElementById('typingError').style.display="block";
+    }
+/*rozważyć polskie znaki!*/
 });
