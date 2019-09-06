f4563059aeb20c9c645237abf247f1ddad23b021
czarny-kot <53179548+czarny-kot@users.noreply.github.com>
2019-09-06 21:49:53 +0200

    Dodanie 'Ł'

--- APICommunication.js
+++ APICommunication.js
@@ -6,8 +6,8 @@ function APIRequestByGeoCoordinates(lat, lon)
     return request;
 }
 function correctPolishLetters (string) {
-    var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
-    return string.replace(/[ąćęłńóśźż]/g, match => dict[match]);
+    var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z', 'Ł':'L'};
+    return string.replace(/[ąćęłńóśźżŁ]/g, match => dict[match]);
   }
 
 function APIRequestByCityName(city)
