e4b95929f8efee6b6b88146ce585c69766f64d35
Krystian <kry.kjjk@op.pl>
2019-09-08 13:01:22 +0200

    small fix

--- Forecast.js
+++ Forecast.js
@@ -12,25 +12,18 @@ const dayName = (dayNr)=>{
     switch(dayNr){
         case 0:
             return 'Niedziela';
-            break;
         case 1:
             return 'Poniedziałek';
-            break;
         case 2:
             return 'Wtorek';
-            break;
         case 3:
             return 'Środa';
-            break;
         case 4:
             return 'Czwartek';
-            break;
         case 5:
             return 'Piątek';
-            break;
         case 6:
             return 'Sobota';
-            break;
     }
 }
 
